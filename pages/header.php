d1847638891786b1419d923bdeae0d541fe57c2f
nguyenphuctan2662002 <nptan_21th@student.agu.edu.vn>
2023-12-01 13:42:20 +0700

    Update header.php

--- pages/header.php
+++ pages/header.php
@@ -10,3 +10,26 @@
 		unset($_SESSION['dangky']);
 	} 
 ?>
+<div class="menu">
+               
+			   <ul class="menu_list">
+				   <li> <a href="index.php">Home</a></li>
+				   <li> <a href="index.php?quanly=contact">Liên hệ </a></li>
+				   <li> <a href="index.php?quanly=giohang">Giỏ hàng</a></li>
+				   <li><a href="">Danh mục</a>
+						   <ul class="menu_danhmuc">
+						   <?php
+									   while($row_danhmuc=mysqli_fetch_array($query_danhmuc)){
+   
+								   ?>
+								   <li> <a href="index.php?quanly=danhmuclist&id=<?php echo $row_danhmuc['id_danhmuc'] ?>"><?php echo $row_danhmuc['tendanhmuc']?></a></li>
+   
+								   <?php
+									   }
+   
+								   ?>
+						   </ul>
+				   </li>
+			   
+				  
+				   <?php
